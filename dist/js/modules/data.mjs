5fe30c0543a5cd92e6940cd09868f96fb5d2afbf
Asad Mujumder <asaduzzaman.anik.me@gmail.com>
2022-10-12 16:11:14 +0600

    changed all the images into webp

--- dist/js/modules/data.mjs
+++ dist/js/modules/data.mjs
@@ -3,7 +3,7 @@ export const certificates = [
         type: 'Frontend',
         name: 'Responsive Web Design',
         provider: 'FreeCodeCamp',
-        imgURL: '/dist/assets/images/certificates/fcc/certificate-fcc-rwd.jpg',
+        imgURL: '/dist/assets/images/certificates/fcc/certificate-fcc-rwd.webp',
         logo: '/dist/assets/images/logo/fcc.png',
         description: 'I completed the Responsive Web Design Course from FreeCodeCamp in 2021. It consists of 300 hours of learning materials. Besides it I also learn the concepts of HTML5 and CSS3 from MDN and W3schools , YouTube, etc and have done a lot of projects. You can see the projects on my github account and also you can see the projects related to this course by following the link to the certificate. You can also verify the certificate from there. My projects are also listed in the Project Section here and you can visit them by following the link provided with them.',
         issued: 'October 25, 2021',
@@ -14,7 +14,7 @@ export const certificates = [
         type: 'Frontend',
         name: 'JavaScript Algorithms and Data Structures',
         provider: 'FreeCodeCamp',
-        imgURL: '/dist/assets/images/certificates/fcc/certificate-fcc-js.jpg',
+        imgURL: '/dist/assets/images/certificates/fcc/certificate-fcc-js.webp',
         logo: '/dist/assets/images/logo/fcc.png',
         description: 'I completed the JavaScript Algorithms and Data Structures Course from FreeCodeCamp in 2022. It consists of 300 hours of learning materials. Besides it I also learn the concepts of Javascript and its various libraries from MDN, W3schools, YouTube, etc and have done a lot of projects. You can see the projects on my GitHub account and also you can see the projects related to this course by following the link to the certificate. You can also verify the certificate from there. My projects are also listed in the Project Section here and you can visit them by following the link provided with them.',
         issued: 'July 8, 2022',
@@ -25,7 +25,7 @@ export const certificates = [
         type: 'Backend',
         name: 'Back End Development and APIs',
         provider: 'FreeCodeCamp',
-        imgURL: '/dist/assets/images/certificates/fcc/certificate-fcc-backend.jpg',
+        imgURL: '/dist/assets/images/certificates/fcc/certificate-fcc-backend.webp',
         logo: '/dist/assets/images/logo/fcc.png',
         description: 'I completed the Back End Development and APIs Course from FreeCodeCamp in 2022. It consists of 300 hours of learning materials. Besides it I also learn the concepts of Node.js, Express.js etc from their respective documentation, MDN, YouTube, etc and have done a lot of projects. You can see the projects on my GitHub account and also you can see the projects related to this course by following the link to the certificate. You can also verify the certificate from there. My projects are also listed in the Project Section here and you can visit them by following the link provided with them.',
         issued: 'July 17, 2022',
@@ -38,7 +38,7 @@ export const projects = [
     {
         type: ['fullstack','mern','react','node'],
         title: 'Memories Social Media App',
-        imgURL: '/dist/assets/images/portfolio/memories-social-media-app.jpg',
+        imgURL: '/dist/assets/images/portfolio/memories-social-media-app.webp',
         link: 'http://am-memories.herokuapp.com',
         githubLink: {
             frontend: 'https://github.com/Asad13/memories-frontend',
@@ -50,7 +50,7 @@ export const projects = [
     {
         type: ['frontend','javascript','svg'],
         title: 'Balloon Structure Designing Tool',
-        imgURL: '/dist/assets/images/portfolio/balloon-design-tool.jpg',
+        imgURL: '/dist/assets/images/portfolio/balloon-design-tool.webp',
         link: 'https://asad13.github.io/Balloon-structure-design-tool/',
         githubLink: {
             frontend: 'https://github.com/Asad13/Balloon-structure-design-tool',
@@ -61,7 +61,7 @@ export const projects = [
     {
         type: ['frontend','next','react','Tailwind'],
         title: 'Tech Blog Using Next.js',
-        imgURL: '/dist/assets/images/portfolio/nextjs-blog-markdown.jpg',
+        imgURL: '/dist/assets/images/portfolio/nextjs-blog-markdown.webp',
         link: 'https://nextjs-blog-markdown-bay.vercel.app/',
         githubLink: {
             frontend: 'https://github.com/Asad13/nextjs-blog-markdown',
